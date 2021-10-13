464dc80b2989b14bff8eb1374fc60d13dd9294a1
rahmathidayatmajid <rahmat@pajanlom.com>
2021-10-13 22:53:26 +0700

    + delete authAdmin, Validate input

--- routes/weightMeasures.js
+++ routes/weightMeasures.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const weightMeasures = require('../controllers/weightMeasuresController')
+const { loginCheck } = require('../middlewares/authentication')
+const { authAdmin } = require('../middlewares/authorization')
 
-router.post('/', weightMeasures.postWeight)
-router.get('/', weightMeasures.getWeight)
+router.post('/', loginCheck, weightMeasures.postWeight)
+router.get('/', loginCheck, weightMeasures.getWeight)
+router.put('/:id', loginCheck, weightMeasures.updateWeight)
+router.delete('/:id', loginCheck, authAdmin, weightMeasures.deleteWeight)
 
 module.exports = router
