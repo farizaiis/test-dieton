f35a5da05b51dc520f017f96881b4ef862489a3d
farizaiis <46044060+farizaiis@users.noreply.github.com>
2021-10-15 12:16:02 +0700

    Update mealsPlansRouter.js

--- routes/mealsPlansRouter.js
+++ routes/mealsPlansRouter.js
@@ -5,7 +5,7 @@ const { loginCheck } = require('../middlewares/authentication')
 
 router.post('/', loginCheck, mealsPlans.postMealsPlans)
 router.get('/', loginCheck, mealsPlans.getUserPlans)
-router.put('/status/:id', loginCheck, mealsPlans.updateStatus)
+router.put('/status/', loginCheck, mealsPlans.updateStatus)
 router.delete('/:id', loginCheck, mealsPlans.deleteMealsPlans)
 
 module.exports = router
