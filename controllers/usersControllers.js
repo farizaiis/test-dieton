7f5083ee0f5a445bba41e44db37be685e9957191
farizaiis <farizaiis@hotmail.com>
2021-10-19 07:24:19 +0700

    ! modify date

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -72,7 +72,7 @@ module.exports = {
                 userId: createUser.dataValues.id,
                 calConsumed: 0,
                 remainCalSize: body.calorieSize,
-                date: moment.utc(new Date()).local().format("YYYY-MM-D")
+                date: moment.utc(new Date()).local().format("LL")
             });
 
             const createWeightMeasure = await weightMeasures.create({
@@ -80,7 +80,7 @@ module.exports = {
                 weight: body.weight,
                 waistline: body.waistline,
                 thigh: body.thigh,
-                date: moment.utc(new Date()).local().format("YYYY-MM-D")
+                date: moment.utc(new Date()).local().format("LL")
             })
 
 
