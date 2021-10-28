487d80b843aaf9686fb0f13d80a79a32ab11ecf2
farizaiis <farizaiis@hotmail.com>
2021-10-28 15:40:55 +0700

    ! controller mealsplans

--- controllers/mealsPlansController.js
+++ controllers/mealsPlansController.js
@@ -39,7 +39,7 @@ module.exports = {
                 where: {
                     userId : req.users.id,
                     date : body.date,
-                    mealsTime : body.date
+                    mealsTime : body.mealsTime
                 }
             })
 
